f5c69ab09ceed94fc990d0ae4cf2fb9b5e080aa1
pubuduGimhana <pubudu200105@gmail.com>
2024-06-01 12:19:42 +0530

    Update modifiedBubleSort.java

--- modifiedBubleSort.java
+++ modifiedBubleSort.java
@@ -1,50 +1,31 @@
-package sortingAlgorithm;
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Track if any swaps were made during this pass
+        swapped = False
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap the elements
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        # If no swaps were made, the array is already sorted
+        if not swapped:
+            break
 
-import java.util.Scanner;
 
-public class modifiedBubleSort {
+def main():
+    # Get user input
+    user_input = input("Enter the  numbers separated by spaces: ")
 
-    public static void bubbleSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            // Track if any swaps were made during this pass
-            boolean swapped = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    // Swap the elements
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
-                }
-            }
-            // If no swaps were made, the array is already sorted
-            if (!swapped) {
-                break;
-            }
-        }
-    }
+    # Convert input string to a list of integers
+    arr = list(map(int, user_input.split()))
 
-    public static void main(String[] args) {
-        // Create a Scanner object to read input
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of elements you want to sort: ");
-        int n = scanner.nextInt();
+    # Perform bubble sort
+    bubble_sort(arr)
 
-        // Create an array of the specified size
-        int[] arr = new int[n];
-        System.out.println("Enter " + n + " integers: ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
+    # Print the sorted array
+    print("Sorted  array:", arr)
 
-        // Perform bubble sort
-        bubbleSort(arr);
 
-        // Print the sorted array
-        System.out.println("Sorted array: ");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
-}
+if __name__ == "__main__":
+    main()
